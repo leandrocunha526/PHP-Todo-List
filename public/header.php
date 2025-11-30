0d9136bcc78864a54279c357001779d90b738165
Leandro Cunha <leandrocunhaemail@gmail.com>
2025-11-30 20:03:35 -0300

    chore: Change html tag title and nav title

--- public/header.php
+++ public/header.php
@@ -12,14 +12,14 @@ $messages = flash_get() ?? [];
 
 <head>
     <meta charset="UTF-8">
-    <title>CRUD PHP + PostgreSQL</title>
+    <title>TODO List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body class="">
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-        <a class="navbar-brand text-white" href="/">CRUD</a>
+        <a class="navbar-brand text-white" href="/">TODO List</a>
 
         <div class="ms-auto d-flex align-items-center gap-2">
             <button id="themeToggle" class="btn btn-sm btn-outline-light">🌓 Tema</button>
